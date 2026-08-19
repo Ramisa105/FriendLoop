dc02f40a3f1d9da6eed93093bb4035373f6e5b51
sadikaTabassum <ssadika.tabassum@gmail.com>
2026-08-19 10:24:59 +0600

    fixed the matches

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import Register from "./pages/Register";
 import Discover from "./pages/Discover"; // The swipe page (old Home)
 import Profile from "./pages/Profile";
 import Admin from "./pages/Admin";
+import Matches from "./pages/Matches";
 
 function App() {
   return (
@@ -22,25 +23,47 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
 
-            {/* Protected Routes */}
-            <Route
-              path="/discover"
-              element={
-                <ProtectedRoute>
-                  <Discover />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/admin" element={<Admin />} />
-            <Route
-              path="/profile"
-              element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
+        <div
+          className={
+            showIntro ? "opacity-0" : "opacity-100 animate-fade-in-app"
+          }
+        >
+          <Navbar />
+          <div className="min-h-screen bg-[#FDF6F0]">
+            <Routes>
+              {/* Public Routes */}
+              <Route path="/" element={<Landing />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+
+              {/* Protected Routes */}
+              <Route
+                path="/discover"
+                element={
+                  <ProtectedRoute>
+                    <Discover />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="/matches"
+                element={
+                  <ProtectedRoute>
+                    <Matches />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/profile"
+                element={
+                  <ProtectedRoute>
+                    <Profile />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </div>
         </div>
       </BrowserRouter>
     </AuthProvider>
