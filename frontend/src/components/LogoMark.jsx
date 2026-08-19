3a732baf4f5e58885408dffa7cd5547ceff389ad
sadikaTabassum <ssadika.tabassum@gmail.com>
2026-08-19 10:53:57 +0600

    save current work before merging main

---
+++ frontend/src/components/LogoMark.jsx
@@ -0,0 +1,6 @@
+import logoImage from "../assets/FriendLoop_Logo.png";
+const LogoMark = ({ className = "" }) => (
+  <img src={logoImage} alt="FriendLoop logo" className={className} />
+);
+
+export default LogoMark;
